c22366a921d2f5ee742922f3aea3946ba6fb34b1
Chris Bibby <cmbibby@gmail.com>
2021-05-16 12:19:56 +0930

    composer depsa

--- plugin.php
+++ plugin.php
@@ -12,7 +12,7 @@
  * Plugin URI:        https://github.com/humanmade/amf-wordpress
  * GitHub Plugin URI: australian-paving/amf-wordpress
  * Description:       Use another WordPress site as source for your media library.
- * Version:           0.1.3
+ * Version:           0.1.4
  * Requires at least: 2.8
  * Requires PHP:      7.2
  * Author:            Human Made
